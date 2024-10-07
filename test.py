2f885826fdf20467612104e62879a276319a620f
Connor Gibbons <connorgibbons@comcast.net>
2024-10-07 10:17:47 -0400

    Binary Addressed Message tests (type 6)

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import unittest
 import ais_decoder
 import math
 
-class TestAISDecoder(unittest.TestCase):
+class test_AIS_decoder(unittest.TestCase):
     def assert_close(self, a, b, abs_tol=0.1):
         if not math.isclose(a, b, abs_tol=abs_tol):
             raise AssertionError(f"{a} and {b} are not close enough")
         
-class TestAISDecodeCNB(TestAISDecoder):
+class test_decode_position_report_class_a(test_AIS_decoder):
     def setUp(self):
         self.testMessage = "!AIVDM,1,1,,A,13QWhR012COJ`0TDSdkCS2ph0@=j,0*6C"
         self.testMessage2 = "!AIVDM,1,1,,B,11mg=5OP00Pdu`JI>lS59Ov<0<0g,0*49"
@@ -50,7 +50,7 @@ class TestAISDecodeCNB(TestAISDecoder):
         self.assertEqual(self.aisMessage2.payload_info["True Heading"], 511)
 
 
-class TestAISDecodeBSR(TestAISDecoder):
+class test_decode_base_station_report(test_AIS_decoder):
     def setUp(self):
         self.testMessage = "!AIVDM,1,1,,B,403t?hAuho;N>`Pc:j>Kgq700D2D,0*2C"
         self.testMessage2 = "!AIVDM,1,1,,A,4020tPiuho;N@PoHAPO027G00@9S,0*5B"
@@ -105,7 +105,7 @@ class TestAISDecodeBSR(TestAISDecoder):
         self.assertEqual(self.aisMessage.payload_info["RAIM Flag"], 0)
         self.assertEqual(self.aisMessage2.payload_info["RAIM Flag"], 0)
 
-class TestAISDecodeVRD(TestAISDecoder):
+class test_decode_static_and_voyage_data(test_AIS_decoder):
     def setUp(self):
         self.testMessages = ['!AIVDM,2,1,5,A,53uuBt02<Tg1<<Tv220HTpplThj222222222221?1rc<>Ho<0@0TQCADR0EQ,0*58', '!AIVDM,2,2,5,A,C`888888880,2*02']
         self.testMessages2 = ['!AIVDM,2,1,6,A,55S:>H000000Q3CGW:1@Dp@E:0EQ18E=>222220j1@62240Ht5RBSEBA1C`8,0*1B', '!AIVDM,2,2,6,A,88888888880,2*22']
@@ -180,5 +180,41 @@ class TestAISDecodeVRD(TestAISDecoder):
         self.assertEqual(self.aisMessage.payload_info["Destination"], "BREMERHAVEN")
         self.assertEqual(self.aisMessage2.payload_info["Destination"], "IJMUIDEN")
 
+class test_decode_binary_addressed_message(test_AIS_decoder):
+    def setUp(self):
+        self.testMessage = "!AIVDM,1,1,,A,6h2E:p66B2SR04<0@00000000000,0*4C"
+        self.testMessage2 = "!AIVDM,1,1,,A,601uEP@tH;3j<P<j00,4*51"
+        self.aisMessage = ais_decoder.AISMessage(self.testMessage).decode()
+        self.aisMessage2 = ais_decoder.AISMessage(self.testMessage2).decode()
+
+    def test_decode_MMSI(self):
+        self.assertEqual(self.aisMessage.payload_info["MMSI"], 2444000)
+        self.assertEqual(self.aisMessage2.payload_info["MMSI"], 2053505)
+    
+    def test_decode_sequence_number(self):
+        self.assertEqual(self.aisMessage.payload_info["Sequence Number"], 1)
+        self.assertEqual(self.aisMessage2.payload_info["Sequence Number"], 0)
+
+    def test_decode_destination_MMSI(self):
+        self.assertEqual(self.aisMessage.payload_info["Destination MMSI"], 563219000)
+        self.assertEqual(self.aisMessage2.payload_info["Destination MMSI"], 253242428)
+
+    def test_decode_retransmit_flag(self):
+        self.assertEqual(self.aisMessage.payload_info["Retransmit Flag"], 1)
+        self.assertEqual(self.aisMessage2.payload_info["Retransmit Flag"], 1)
+
+    def test_decode_designated_area_code(self):
+        self.assertEqual(self.aisMessage.payload_info["Designated Area Code"], 1)
+        self.assertEqual(self.aisMessage2.payload_info["Designated Area Code"], 200)
+    
+    def test_functional_ID(self):
+        self.assertEqual(self.aisMessage.payload_info["Functional ID"], 3)
+        self.assertEqual(self.aisMessage2.payload_info["Functional ID"], 3)
+    
+    def test_data(self):
+        self.assertEqual(self.aisMessage.payload_info["Data"], "@D@@@@@@@@@@@")
+        self.assertEqual(self.aisMessage2.payload_info["Data"], "L @")
+
+    
 if __name__ == '__main__':
     unittest.main()
